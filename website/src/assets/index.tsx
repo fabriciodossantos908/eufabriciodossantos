61578b2be87e0c11c4f3ac64ca0fde32c6934fd0
fabriciodossantos908 <fabriciodossantos908@gmail.com>
2022-10-13 01:43:45 -0300

    Feat: images import control

---
+++ website/src/assets/index.tsx
@@ -0,0 +1,9 @@
+import github from "./github.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import wave from "./wave.svg";
+import bg from "./bg.svg";
+import downArrow from "./downArrow.svg";
+
+
+export {github, instagram, linkedin, wave, bg, downArrow};
